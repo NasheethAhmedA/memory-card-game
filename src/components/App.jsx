eb95a8d25680651523623bc70c5aa8a9dd629d4b
Nasheeth Ahmed A <nasheethahmeda04@gmail.com>
2024-03-30 22:10:07 +0530

    Final adjustments
    Added npm build config changes
    ReadMe file update
    demo link

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,13 +3,15 @@ import { useStopwatch } from "react-timer-hook";
 import "./App.css";
 import CardGrid from "./CardGrid";
 
-const selectAudio = new Audio("sfx/select.wav")
+// Define audio files and set their volume
+const selectAudio = new Audio("sfx/select.wav");
 selectAudio.volume = 0.5;
 const matchAudio = new Audio("sfx/match.wav");
 matchAudio.volume = 0.5;
 const winAudio = new Audio("sfx/win.wav");
 winAudio.volume = 0.5;
 
+// Define an array of card images with their matched status
 const CardImages = [
   { src: "images/Book.png", matched: false },
   { src: "images/Helmet.png", matched: false },
@@ -22,21 +24,25 @@ const CardImages = [
 ];
 
 function App() {
+  // Define state variables
   const [cards, setCards] = useState([]);
   const [turns, setTurns] = useState(0);
   const [choice1, setChoice1] = useState(null);
   const [choice2, setChoice2] = useState(null);
   const winScreenRef = useRef(null);
 
+  // Define stopwatch using the useStopwatch hook
   const { seconds, minutes, hours, isRunning, start, pause, reset } =
     useStopwatch({ autoStart: true });
 
+  // Handle user's card choice
   const handleChoice = (card) => {
     selectAudio.play();
     choice1 ? setChoice2(card) : setChoice1(card);
     setTurns((prevTurns) => prevTurns + 1);
   };
 
+  // Check if all cards are matched and display win screen
   useEffect(() => {
     if (cards.every((card) => card.matched)) {
       winAudio.play();
@@ -45,6 +51,7 @@ function App() {
     }
   }, [cards]);
 
+  // Check if the two chosen cards match
   useEffect(() => {
     if (choice1 && choice2) {
       if (choice1.src === choice2.src) {
@@ -66,18 +73,18 @@ function App() {
     }
   }, [choice1, choice2]);
 
-  //reset choices
+  // Reset the chosen cards
   const resetChoices = () => {
     setChoice1(null);
     setChoice2(null);
   };
 
-  // Shuffle the cards
+  // Shuffle the cards and start the game
   const shuffleCards = () => {
     winScreenRef.current.style.display = "none";
     const shuffledCards = [...CardImages, ...CardImages]
       .sort(() => Math.random() - 0.5)
-      .map((card) => ({ ...card, id: Math.random() }));
+      .map((card) => ({ ...card, id: Math.random() - 0.5}));
 
     setCards(shuffledCards);
     setChoice1(null);
@@ -86,11 +93,13 @@ function App() {
     reset();
   };
 
+  // Shuffle the cards and start the game when the component mounts
   useEffect(() => {
     shuffleCards();
     start();
   }, []);
 
+  // Render the app component
   return (
     <div className="App">
       <h1>Memory Card Game</h1>
